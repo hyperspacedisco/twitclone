f900a15d9f1bb3f5023f0444f49e450a792d8cbb
Sarah Lyne <lynesarah@rocketmail.com>
2016-02-09 13:47:44 +1300

    add navigation to master

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,6 +7,15 @@
 </head>
 <body>
 
+<nav>
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="/contact">Contact</a></li>
+		<li><a href="/register">Register with us</a></li>
+		<li><a href="/login">Login</a></li>
+	</ul>
+</nav>
+
 	@yield('content')
 
 
