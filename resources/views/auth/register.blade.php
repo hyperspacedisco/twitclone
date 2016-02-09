f6dff5d86883c3f7b75808cfeab13d1aba252caa
Sarah Lyne <lynesarah@rocketmail.com>
2016-02-09 14:50:23 +1300

    add working registration form with inbuilt laravel registration functionality

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,4 +6,40 @@
 @section('content')
 
 <h1>Register With Us</h1>
+
+<form action="/register" method="post">
+
+	{!! csrf_field() !!}
+
+	<div>
+		<label for="name">Full Name: </label>
+		<input type="text" name="name" id="name" placeholder="Elton John">
+	</div>
+
+	<div>
+		<label for="email">Email Address: </label>
+		<input type="email" name="email" id="email" placeholder="name@email.com">
+	</div>
+
+	<div>
+		<label for="password">Password: </label>
+		<input type="password" name="password" id="password">
+	</div>
+
+	<div>
+		<label for="password_confirmation">Confirm Password: </label>
+		<input type="password" name="password_confirmation" id="password_confirmation">
+	</div>
+
+	<input type="submit" value="Register">
+</form>
+
+@if(count($errors))
+<ul>
+	@foreach($errors->all() as $error)
+		<li>{{ $error }}</li>
+	@endforeach
+</ul>	
+	
+@endif
 @endsection
