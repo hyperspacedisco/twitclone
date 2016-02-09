f7885cd2cccd3f22888e70e723d891bca2476dc3
Sarah Lyne <lynesarah@rocketmail.com>
2016-02-09 14:01:09 +1300

    add register view

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,9 @@
+@extends('master')
+
+@section('title', 'Register')
+@section('meta-description', 'Register with us to start sharing your thoughts')
+
+@section('content')
+
+<h1>Register With Us</h1>
+@endsection
